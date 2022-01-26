15151eeb1b2374e98148635e29cfcdfe10fcc616
Miller327C <tandrey.2002@mail.ru>
2022-01-26 20:46:19 +0600

    First News added

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 const apiKey = 'f778c165f85a42a0b73e0877131f2edb';
 const container = document.querySelector('.news');
 
+
+
 async function getNews(api) {
     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=ru&apiKey=${api}`);
     const requestValue = response.json();
@@ -8,29 +10,43 @@ async function getNews(api) {
 }
 
 function showNews(responseData) {
+    let num = 0;
+
     const articles = responseData.articles;
     articles.forEach(({title, description, urlToImage, url}) => {
-        const card = document.createElement('div');
-        card.classList.add('col-6');
-        card.classList.add('offset-3');
-        card.classList.add('card');
 
-        const titleText = document.createElement('h2');
-        const descr = document.createElement('p');
-        const img = document.createElement('img')
-        const href = document.createElement('a');
+        if (num === 0) {
+            const fnImg = document.querySelector('.first_news_img');
+            const fnTitle = document.querySelector('.first_news_title');
+            const fnLink = document.querySelector('.first_news_link');
 
-        titleText.textContent = title;
-        descr.textContent = description;
-        img.setAttribute('src', `${urlToImage}`);
-        href.setAttribute('href', `${url}`);
-        href.textContent = 'Read more';
+            fnImg.setAttribute('src', `${urlToImage}`);
+            fnTitle.textContent = title;
+            fnLink.setAttribute('href', `${url}`);
 
-        card.appendChild(img);
-        card.appendChild(titleText);
-        card.appendChild(descr);
-        card.appendChild(href);
-        container.insertAdjacentElement('afterbegin', card);
+            num = 1;
+        }else {
+            const img = document.createElement('img')
+            const href = document.createElement('a');
+            const titleText = document.createElement('h2');
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const descr = document.createElement('p');
+            descr.textContent = description;
+
+            titleText.textContent = title;
+            img.setAttribute('src', `${urlToImage}`);
+            href.setAttribute('href', `${url}`);
+            href.textContent = 'Read more';
+            
+            card.appendChild(img);
+            card.appendChild(titleText);
+            card.appendChild(descr);
+            card.appendChild(href);
+            container.insertAdjacentElement('afterbegin', card); 
+        }
+        
     });
 }
 
